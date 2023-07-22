55d820033b9dc071c674903adf3b4125d0a6cef3
KetchuppOfficial <dolgov04@list.ru>
2023-07-22 15:43:33 +0300

    Driver has been significantly updated: 1) Support of two kinds of trees (std::set and yLab::Splay_Tree) has been added; 2) Measuring time spent of a test has been added; 3) Processing input has been divided into processing keys and queries.

--- test/end_to_end/src/driver.cpp
+++ test/end_to_end/src/driver.cpp
@@ -3,46 +3,57 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <chrono>
+#include <fstream>
 
+#ifdef SPLAY_TREE
 #include "splay_tree.hpp"
+#else
+#include <set>
+#endif
 
 namespace yLab
 {
 
-using key_type = int;
-using tree_type = yLab::Splay_Tree<key_type>;
-using query_type = std::pair<key_type, key_type>;
-
-std::pair<tree_type, std::vector<query_type>> get_tree_and_queries ()
+template<typename Key_T>
+std::vector<Key_T> get_keys ()
 {
-    std::size_t n_elems;
-    std::cin >> n_elems;
+    std::size_t n_keys;
+    std::cin >> n_keys;
     if (!std::cin.good())
-        throw std::runtime_error{"Error while reading the number of elements"};
+        throw std::runtime_error{"Error while reading the number of keys"};
 
-    tree_type tree;
-    for (auto elem_i = 0; elem_i != n_elems; ++elem_i)
+    std::vector<Key_T> keys;
+    keys.reserve (n_keys);
+
+    for (auto key_i = 0; key_i != n_keys; ++key_i)
     {
-        key_type elem;
-        std::cin >> elem;
+        Key_T key;
+        std::cin >> key;
         if (!std::cin.good())
-            throw std::runtime_error{"Error while reading elements"};
+            throw std::runtime_error{"Error while reading keys"};
 
-        tree.insert (elem);
+        keys.emplace_back (key);
     }
 
+    return keys;
+}
+
+template<typename Key_T>
+std::vector<std::pair<Key_T, Key_T>> get_queries ()
+{
     std::size_t n_queries;
     std::cin >> n_queries;
     if (!std::cin.good())
         throw std::runtime_error{"Error while reading the number of queries"};
 
-    std::vector<query_type> queries;
+    std::vector<std::pair<Key_T, Key_T>> queries;
     queries.reserve (n_queries);
 
     for (auto query_i = 0; query_i != n_queries; ++query_i)
     {
-        key_type lower_bound;
-        key_type upper_bound;
+        Key_T lower_bound;
+        Key_T upper_bound;
 
         std::cin >> lower_bound >> upper_bound;
         if (std::cin.fail())
@@ -51,19 +62,41 @@ std::pair<tree_type, std::vector<query_type>> get_tree_and_queries ()
         queries.emplace_back (lower_bound, upper_bound);
     }
 
-    return std::pair{tree, queries};
+    return queries;
 }
 
 } // namespace yLab
 
 int main ()
 {
-    auto [tree, queries] = yLab::get_tree_and_queries();
+    using key_type = int;
+    
+    auto keys = yLab::get_keys<key_type>();
+    auto queries = yLab::get_queries<key_type>();
+
+    #ifdef SPLAY_TREE
+    yLab::Splay_Tree<key_type> tree (keys.begin(), keys.end());
+    #else
+    std::set<key_type> tree (keys.begin(), keys.end());
+    #endif
+
+    auto start = std::chrono::high_resolution_clock::now();
 
     std::for_each (queries.begin(), queries.end(),
                    [&tree](auto &&pair){ std::cout << std::distance (tree.lower_bound (pair.first),
                                                                      tree.upper_bound (pair.second)) << " "; });
+
     std::cout << std::endl;
+
+    auto finish = std::chrono::high_resolution_clock::now();
+
+    #ifdef SPLAY_TREE
+    std::ofstream file{"splay_tree.info"};
+    #else
+    std::ofstream file{"std_set.info"};
+    #endif
+
+    file << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count() << std::endl;
 
     return 0;
 }
