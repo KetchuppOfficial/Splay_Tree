116dd4562b990e40c8892c5ff394720d34638abe
KetchuppOfficial <dolgov04@list.ru>
2024-02-28 00:18:36 +0300

    Some codestyle changes and more C++20 ranges

--- test/end_to_end/src/driver.cpp
+++ test/end_to_end/src/driver.cpp
@@ -19,7 +19,7 @@ namespace
 {
 
 template<typename Key_T>
-std::vector<Key_T> get_keys ()
+std::vector<Key_T> get_keys()
 {
     std::size_t n_keys;
     std::cin >> n_keys;
@@ -27,7 +27,7 @@ std::vector<Key_T> get_keys ()
         throw std::runtime_error{"Error while reading the number of keys"};
 
     std::vector<Key_T> keys;
-    keys.reserve (n_keys);
+    keys.reserve(n_keys);
 
     for (auto key_i = 0; key_i != n_keys; ++key_i)
     {
@@ -36,14 +36,14 @@ std::vector<Key_T> get_keys ()
         if (!std::cin.good())
             throw std::runtime_error{"Error while reading keys"};
 
-        keys.push_back (key);
+        keys.push_back(key);
     }
 
     return keys;
 }
 
 template<typename Key_T>
-std::vector<std::pair<Key_T, Key_T>> get_queries ()
+std::vector<std::pair<Key_T, Key_T>> get_queries()
 {
     std::size_t n_queries;
     std::cin >> n_queries;
@@ -51,7 +51,7 @@ std::vector<std::pair<Key_T, Key_T>> get_queries ()
         throw std::runtime_error{"Error while reading the number of queries"};
 
     std::vector<std::pair<Key_T, Key_T>> queries;
-    queries.reserve (n_queries);
+    queries.reserve(n_queries);
 
     for (auto query_i = 0; query_i != n_queries; ++query_i)
     {
@@ -62,34 +62,34 @@ std::vector<std::pair<Key_T, Key_T>> get_queries ()
         if (std::cin.fail())
             throw std::runtime_error{"Error while reading queries"};
 
-        queries.emplace_back (lower_bound, upper_bound);
+        queries.emplace_back(lower_bound, upper_bound);
     }
 
     return queries;
 }
 
 template<typename Tree_T, std::forward_iterator It>
-std::vector<std::size_t> run_test (const Tree_T &tree, It from, It to)
+std::vector<std::size_t> run_test(const Tree_T &tree, It from, It to)
 {
     std::vector<std::size_t> answers;
-    answers.reserve (std::distance (from, to));
+    answers.reserve(std::distance(from, to));
 
-    std::for_each (from, to, [&tree, &answers](auto &&pair)
+    std::for_each(from, to, [&tree, &answers](auto &&pair)
     {
         if (pair.first <= pair.second)
         {
             std::size_t n_elems;
 
             if constexpr (yLab::contains_subtree_size<typename Tree_T::node_type>)
-                n_elems = tree.n_less_or_equal_to (pair.second) - tree.n_less_than (pair.first);
+                n_elems = tree.n_less_or_equal_to(pair.second) - tree.n_less_than(pair.first);
             else
-                n_elems = std::distance (tree.lower_bound (pair.first),
-                                         tree.upper_bound (pair.second));
+                n_elems = std::distance(tree.lower_bound(pair.first),
+                                        tree.upper_bound(pair.second));
 
-            answers.push_back (n_elems);
+            answers.push_back(n_elems);
         }
         else
-            answers.push_back (0);
+            answers.push_back(0);
     });
 
     return answers;
@@ -97,29 +97,29 @@ std::vector<std::size_t> run_test (const Tree_T &tree, It from, It to)
 
 } // unnamed namespace
 
-int main ()
+int main()
 {
     using key_type = int;
 
     auto keys = get_keys<key_type>();
     auto queries = get_queries<key_type>();
 
-    #if defined(AUGMENTED_SPLAY_TREE)
-    yLab::Augmented_Splay_Tree<key_type> tree (keys.begin(), keys.end());
+#if defined(AUGMENTED_SPLAY_TREE)
+    yLab::Augmented_Splay_Tree<key_type> tree{keys.begin(), keys.end()};
     std::ofstream file{"augmented_splay_tree.info"};
-    #elif defined(SPLAY_TREE)
-    yLab::Splay_Tree<key_type> tree (keys.begin(), keys.end());
+#elif defined(SPLAY_TREE)
+    yLab::Splay_Tree<key_type> tree{keys.begin(), keys.end()};
     std::ofstream file{"splay_tree.info"};
-    #else
-    std::set<key_type> tree (keys.begin(), keys.end());
+#else
+    std::set<key_type> tree{keys.begin(), keys.end()};
     std::ofstream file{"std_set.info"};
-    #endif
+#endif
 
     auto start = std::chrono::high_resolution_clock::now();
-    auto answers = run_test (tree, queries.begin(), queries.end());
+    auto answers = run_test(tree, queries.begin(), queries.end());
     auto finish = std::chrono::high_resolution_clock::now();
 
-    std::copy (answers.begin(), answers.end(), std::ostream_iterator<std::size_t>{std::cout, " "});
+    std::ranges::copy(answers, std::ostream_iterator<std::size_t>{std::cout, " "});
     std::cout << std::endl;
 
     file << std::chrono::duration_cast<std::chrono::milliseconds>(finish - start).count()
--- test/end_to_end/src/generator.cpp
+++ test/end_to_end/src/generator.cpp
@@ -11,16 +11,16 @@
 namespace yLab
 {
 
-std::pair<std::size_t, std::size_t> get_cmd_line_args (int argc, char *argv[])
+std::pair<std::size_t, std::size_t> get_cmd_line_args(int argc, char *argv[])
 {
     if (argc != 3)
         throw std::runtime_error{"Program requires 2 arguments"};
 
-    auto n_keys = std::atoi (argv[1]);
+    auto n_keys = std::atoi(argv[1]);
     if (n_keys < 0)
         throw std::runtime_error{"The number of keys has to be a positive integer"};
 
-    auto n_queries = std::atoi (argv[2]);
+    auto n_queries = std::atoi(argv[2]);
     if (n_queries < 0)
         throw std::runtime_error{"The number of queries has to be a positive integer"};
 
@@ -28,34 +28,34 @@ std::pair<std::size_t, std::size_t> get_cmd_line_args (int argc, char *argv[])
 }
 
 template<typename Key_T, typename Distr_T, typename Engine>
-std::pair<Key_T, Key_T> generate_keys (std::size_t n_keys, Distr_T distr, Engine gen)
+std::pair<Key_T, Key_T> generate_keys(std::size_t n_keys, Distr_T distr, Engine gen)
 {
     std::vector<Key_T> keys;
-    keys.reserve (n_keys);
+    keys.reserve(n_keys);
 
     for (auto key_i = 0; key_i != n_keys; ++key_i)
-        keys.emplace_back (distr (gen));
+        keys.emplace_back(distr(gen));
 
-    auto min = *std::min_element (keys.begin(), keys.end());
-    auto max = *std::max_element (keys.begin(), keys.end());
+    auto min = *std::ranges::min_element(keys);
+    auto max = *std::ranges::max_element(keys);
 
     std::cout << n_keys << std::endl;
-    std::copy (keys.begin(), keys.end(), std::ostream_iterator<Key_T>{std::cout, " "});
+    std::ranges::copy(keys, std::ostream_iterator<Key_T>{std::cout, " "});
     std::cout << std::endl;
 
-    return std::pair{std::midpoint (min, max), (max - min) / 2};
+    return std::pair{std::midpoint(min, max), (max - min) / 2};
 }
 
 template<typename Key_T, typename Distr_T, typename Engine>
-void generate_queries (std::size_t n_queries, Distr_T distr, Engine gen)
+void generate_queries(std::size_t n_queries, Distr_T distr, Engine gen)
 {
     std::cout << n_queries << std::endl;
     for (auto query_i = 0; query_i != n_queries; ++query_i)
     {
-        Key_T lower_bound = std::round (distr (gen));
-        Key_T upper_bound = std::round (distr (gen));
+        Key_T lower_bound = std::round(distr(gen));
+        Key_T upper_bound = std::round(distr(gen));
         if (lower_bound > upper_bound)
-            std::swap (lower_bound, upper_bound);
+            std::swap(lower_bound, upper_bound);
 
         std::cout << lower_bound << " " << upper_bound << " ";
     }
@@ -64,20 +64,20 @@ void generate_queries (std::size_t n_queries, Distr_T distr, Engine gen)
 
 } // namespace yLab
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     using key_type = int;
 
-    auto [n_keys, n_queries] = yLab::get_cmd_line_args (argc, argv);
+    auto [n_keys, n_queries] = yLab::get_cmd_line_args(argc, argv);
 
     std::random_device rd;
     std::mt19937_64 gen{rd()};
 
     std::uniform_int_distribution<key_type> key{};
-    auto [mean, stddef] = yLab::generate_keys<key_type> (n_keys, key, gen);
+    auto [mean, stddef] = yLab::generate_keys<key_type>(n_keys, key, gen);
 
     std::normal_distribution query{static_cast<double>(mean), static_cast<double>(stddef)};
-    yLab::generate_queries<key_type> (n_queries, query, gen);
+    yLab::generate_queries<key_type>(n_queries, query, gen);
 
     return 0;
 }
